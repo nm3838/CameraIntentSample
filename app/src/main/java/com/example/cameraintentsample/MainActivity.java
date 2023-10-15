390454483ca41aa5f17ac62a74c4a0520e54d75a
nm3838 <qpzorongee.nao.31@outlook.jp>
2023-10-15 13:52:00 +0900

    アプリ間の画像の連携をストレージ経由での連携に変更

--- app/src/main/java/com/example/cameraintentsample/MainActivity.java
+++ app/src/main/java/com/example/cameraintentsample/MainActivity.java
@@ -6,14 +6,23 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    //保存された画像のURI
+    private Uri _imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +36,40 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         //カメラアプリとの連携からの戻りでかつ撮影成功の場合
         if(requestCode == 200 && resultCode == RESULT_OK) {
-            //撮影された画像のビットマップデータを取得
-            Bitmap bitmap = data.getParcelableExtra("data");
             //画像を表示するImageViewを取得
             ImageView ivCamera = findViewById(R.id.ivCamera);
             //撮影された画像をImageViewに設定
-            ivCamera.setImageBitmap(bitmap);
+            ivCamera.setImageURI(_imageUri);
         }
     }
 
     //画像部分がタップされた時の処理メソッド
     public void onCameraImageClick(View view) {
+        //日時データを「yyyyMMddHHmmss」の形式に整形するフォーマットを生成
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        //現在の日時を取得
+        Date now = new Date(System.currentTimeMillis());
+        //取得した日時データを「yyyyMMddHHmmss」形式に整形した文字列を作成
+        String nowStr = dateFormat.format(now);
+        //ストレージに格納する画像のファイル名を生成。
+        //ファイル名の一いせいを確保するためにタイムスタンプの値を利用
+        String fileName = "CameraIntentSamplePhoto_"+nowStr+".jpg";
+
+        //ContentValuesオブジェクトを生成
+        ContentValues values = new ContentValues();
+        //画像ファイル名を設定
+        values.put(MediaStore.Images.Media.TITLE, fileName);
+        //画像ファイルの種類を設定
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        //ContentResolverオブジェクトを生成
+        ContentResolver resolver = getContentResolver();
+        //ContentResolverを使ってURIオブジェクトを生成
+        _imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
         //インテントオブジェクトを生成
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Extra情報として_imageUriを設定
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageUri);
         //アクティビティを起動
         startActivityForResult(intent, 200);
         ;
