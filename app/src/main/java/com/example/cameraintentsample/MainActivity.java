793e354bb0a766c5d6b2dd734ebc007e6c494b92
nm3838 <qpzorongee.nao.31@outlook.jp>
2023-10-15 12:30:56 +0900

    カメラ連携アプリの作成

--- app/src/main/java/com/example/cameraintentsample/MainActivity.java
+++ app/src/main/java/com/example/cameraintentsample/MainActivity.java
@@ -21,6 +21,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //親クラスの同名メソッドの呼び出し
+        super.onActivityResult(requestCode, resultCode, data);
+        //カメラアプリとの連携からの戻りでかつ撮影成功の場合
+        if(requestCode == 200 && resultCode == RESULT_OK) {
+            //撮影された画像のビットマップデータを取得
+            Bitmap bitmap = data.getParcelableExtra("data");
+            //画像を表示するImageViewを取得
+            ImageView ivCamera = findViewById(R.id.ivCamera);
+            //撮影された画像をImageViewに設定
+            ivCamera.setImageBitmap(bitmap);
+        }
+    }
+
     //画像部分がタップされた時の処理メソッド
     public void onCameraImageClick(View view) {
         //インテントオブジェクトを生成
